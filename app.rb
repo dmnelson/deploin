f02136e8c11ce1b7b9e15037b4d90e64a8089283
Felipe Casadei <felipe.casadei@gmail.com>
2015-06-11 16:02:55 -0300

    add link to commit

--- app.rb
+++ app.rb
@@ -6,9 +6,16 @@ require_relative "models/deployment"
 
 Dotenv.load
 
+helpers do
+  def commit_url(commit)
+    "#{ENV['REPO_HOST_URL']}/#{ENV['REPO_OWNER']}/#{ENV['REPO_NAME']}/commit/#{commit.sha}"
+  end
+end
+
 get "/" do
   @deployments = Deployment.all
   @current = @deployments.first
+  @repo =
 
   slim :index
 end
