aca0b47aef51f49f20458a10bccaeb2cbef8c281
David Nelson <davidmichael.nelson@gmail.com>
2016-05-24 17:21:46 -0300

    Add ability to parse rollback log entries

--- models/deployment.rb
+++ models/deployment.rb
@@ -1,5 +1,5 @@
-class Deployment < Struct.new(:repo, :branch, :commit_hash, :timestamp, :author)
-  REGEX = /^Branch (?<branch>.+?) \(at (?<commit_hash>\w+)\) deployed as release (?<timestamp>\d+) by (?<author>.+?);/
+class Deployment < Struct.new(:repo, :branch, :commit_hash, :timestamp, :author, :rollback)
+  alias rollback? rollback
 
   def time
     DateTime.parse(timestamp).to_time
@@ -14,12 +14,9 @@ class Deployment < Struct.new(:repo, :branch, :commit_hash, :timestamp, :author)
   end
 
   def self.from_log(repo, log_line)
-    if match = log_line.match(REGEX)
-      data = match.names.map(&:to_sym).zip(match.captures).to_h.merge!(repo: repo)
-      self.new(*data.values_at(*self.members))
-    else
-      raise "Invalid log entry: #{log_line}"
-    end
+    log = DeploymentLog.parse(log_line)
+    data = log.data.merge!(repo: repo, rollback: log.rollback?)
+    self.new(*data.values_at(*self.members))
   end
 
   def self.all(repo)
@@ -30,5 +27,39 @@ class Deployment < Struct.new(:repo, :branch, :commit_hash, :timestamp, :author)
         end
       end
     end
+  end
+
+  class DeploymentLog < Struct.new(:log_line)
+    def capture
+      @_capture ||= log_line.match(self.pattern)
+    end
+
+    def match?
+      !!self.capture
+    end
+
+    def data
+      self.capture.names.map(&:to_sym).zip(self.capture.captures).to_h
+    end
+
+    def self.parse(log_line)
+      [RegularDeployment.new(log_line), Rollback.new(log_line)].find(&:match?) or raise "Invalid log entry: #{log_line}"
+    end
+  end
+
+  class RegularDeployment < DeploymentLog
+    def pattern
+      /^Branch (?<branch>.+?) \(at (?<commit_hash>\w+)\) deployed as release (?<timestamp>\d+) by (?<author>.+?);/
+    end
+
+    def rollback?; false; end
+  end
+
+  class Rollback < DeploymentLog
+    def pattern
+      /^(?<author>.+?); rolled back to release (?<timestamp>\d+)/
+    end
+
+    def rollback?; true; end
   end
 end
